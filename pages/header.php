bb6d22df7eb95127cee943b27149d5109eb1fa33
scroutch <cecile.duhain@gmail.com>
2022-10-17 15:50:57 +0200

    create pages

---
+++ pages/header.php
@@ -0,0 +1,20 @@
+<header>
+    <button type="button" aria-label="toggle curtain navigation" class="nav-toggler">
+        <span class="line l1"></span>
+        <span class="line l2"></span>
+        <span class="line l3"></span>
+    </button>
+    <img src="assets/img/logo.png" alt="">
+    <nav>
+        <ul id="menu-navigation" class="itemsmenu">
+            <li class="menu-item"><a href="index.php?page=1">Accueil</a></li>
+            <li class="menu-item"><a href="index.php?page=2">Locations</a></li>
+            <li class="menu-item"><a href="index.php?page=3">Ventes</a></li>
+            <li class="menu-item"><a href="index.php?page=4">Estimation</a></li>
+            <li class="menu-item"><a href="index.php?page=5">Contact</a></li>
+        </ul>
+    </nav>
+    <div id="langage">
+        <img src="assets/img/icons/german_flag.png" alt="icone de l'allemagne">
+    </div>
+</header>
