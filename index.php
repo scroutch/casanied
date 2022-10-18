e5eb8ef1c20b09d50351d53782dd54841dd87ac1
scroutch <cecile.duhain@gmail.com>
2022-10-18 11:56:46 +0200

    footer and responsive footer done

--- index.php
+++ index.php
@@ -46,14 +46,14 @@ include('pages/bdd.php');
         include('pages/home.php');
     }
 
+    include('pages/footer.php');
     ?>
-    <footer>
 
-    </footer>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="./assets/bootstrap-5.2.2/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
     <script src="assets/js/menuBurger.js"></script>
+    <script src="assets/js/langage.js"></script>
 </body>
 
 </html>
