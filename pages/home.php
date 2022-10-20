219bb08201dfaf1903d1656ba8752223b56d8bf4
scroutch <cecile.duhain@gmail.com>
2022-10-20 17:39:21 +0200

    add form for rent and sell product

--- pages/home.php
+++ pages/home.php
@@ -5,6 +5,7 @@ $query = 'SELECT * FROM product';
 $query = $bdd->prepare($query);
 $query->execute();
 ?>
+<h2>Nos dernières offres</h2>
 <div class="container-fluid d-flex justify-content-evenly flex-wrap">
     <?php
     while ($data = $query->fetch()) {
@@ -13,7 +14,7 @@ $query->execute();
             <img class="card-img-top" src="<?php echo $data['img'] ?>" alt="Card image cap">
             <div class="card-body d-flex flex-column justify-content-around">
                 <h5 class="card-title d-flex justify-content-between"><?php echo $data['title'] ?><span><?php echo $data['price'] ?> €</span></h5>
-                <p class="card-text d-flex justify-content-center"><i class="fa-solid fa-location-dot"></i><span class="ps-2"><?php echo $data['lieu'] ?></span></p>
+                <p class="card-text d-flex justify-content-center"><i class="fa-solid fa-location-dot"></i><span class="ps-2"><?php echo $data['rue'] ?>, <?php echo $data['ville'] ?></span></p>
                 <p class="card-text d-flex justify-content-around">
                     <span class="p-2"><i class="fa-solid fa-bed"></i><span class="ps-2"><?php echo $data['nb_bedroom'] ?></span></span>
                     <span class="p-2"><i class="fa-solid fa-bath"></i><span class="ps-2"><?php echo $data['nb_bathroom'] ?></span></span>
--- pages/onglet.php
+++ pages/onglet.php
@@ -7,48 +7,136 @@
         </div>
         <form action="" method="post" class="tab-body active" data-id="location">
             <p>3500 biens à louer</p>
-            <input type="text" name="lieu" placeholder="Quartier, Ville, ...">
-            <input type="text" name="price" placeholder="Budget max">
-            <input type="text" name="surface" placeholder="surface">
-            <div>
-                <input type="checkbox" name="type" id="maison" value="maison" checked>
-                <label for="maison">Maison</label>
+            <input type="text" class="lieu" name="lieu" placeholder="Quartier, Ville, ...">
+            <div class="inputBudget">
+                <input type="text" class="budget" name="price" placeholder="Budget max">
+                <input type="text" class="budget" name="surface" placeholder="surface">
+            </div>
+            <div class="check">
+                <div>
+                    <input type="checkbox" name="type" id="maison" value="maison" checked>
+                    <label for="maison">Maison</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="appartement" value="appartement">
+                    <label for="appartement">Appartement</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="terrain" value="terrain">
+                    <label for="terrain">Terrain</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="immeuble" value="immeuble">
+                    <label for="immeuble">Immeuble</label>
+                </div>
             </div>
             <div>
-                <input type="checkbox" name="type" id="appartement" value="appartement">
-                <label for="appartement">Appartement</label>
+                <label for="nb-bedroom">Nombre de chambre</label>
+                <select name="nb_bedroom" id="nb_bedroom">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
             </div>
-            <div>
-                <input type="checkbox" name="type" id="terrain" value="terrain">
-                <label for="terrain">Terrain</label>
-            </div>
-            <div>
-                <input type="checkbox" name="type" id="immeuble" value="immeuble">
-                <label for="immeuble">Immeuble</label>
-            </div>
+            <input type="submit" value="CHERCHER">
         </form>
         <form action="" method="post" class="tab-body" data-id="vente">
-            <p>3500 biens à vendre</p>
-            <input type="text" name="lieu" placeholder="Quartier, Ville, ...">
-            <input type="text" name="price" placeholder="Budget max">
-            <input type="text" name="surface" placeholder="surface">
-            <div>
-                <input type="checkbox" name="type" id="maison" value="maison" checked>
-                <label for="maison">Maison</label>
+            <p>3500 biens à louer</p>
+            <input type="text" class="lieu" name="lieu" placeholder="Quartier, Ville, ...">
+            <div class="inputBudget">
+                <input type="text" class="budget" name="price" placeholder="Budget max">
+                <input type="text" class="budget" name="surface" placeholder="surface">
+            </div>
+            <div class="check">
+                <div>
+                    <input type="checkbox" name="type" id="maison" value="maison" checked>
+                    <label for="maison">Maison</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="appartement" value="appartement">
+                    <label for="appartement">Appartement</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="terrain" value="terrain">
+                    <label for="terrain">Terrain</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="immeuble" value="immeuble">
+                    <label for="immeuble">Immeuble</label>
+                </div>
             </div>
             <div>
-                <input type="checkbox" name="type" id="appartement" value="appartement">
-                <label for="appartement">Appartement</label>
+                <label for="nb-bedroom">Nombre de chambre</label>
+                <select name="nb_bedroom" id="nb_bedroom">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
             </div>
-            <div>
-                <input type="checkbox" name="type" id="terrain" value="terrain">
-                <label for="terrain">Terrain</label>
-            </div>
-            <div>
-                <input type="checkbox" name="type" id="immeuble" value="immeuble">
-                <label for="immeuble">Immeuble</label>
-            </div>
+            <input type="submit" value="CHERCHER">
         </form>
-        <form action="" method="post" class="tab-body" data-id="estimation"></form>
+        <form action="" method="post" class="tab-body" data-id="estimation">
+            <p>3500 biens à louer</p>
+            <input type="text" class="lieu" name="lieu" placeholder="Quartier, Ville, ...">
+            <div class="inputBudget">
+                <input type="text" class="budget" name="price" placeholder="Budget max">
+                <input type="text" class="budget" name="surface" placeholder="surface">
+            </div>
+            <div class="check">
+                <div>
+                    <input type="checkbox" name="type" id="maison" value="maison" checked>
+                    <label for="maison">Maison</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="appartement" value="appartement">
+                    <label for="appartement">Appartement</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="terrain" value="terrain">
+                    <label for="terrain">Terrain</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="type" id="immeuble" value="immeuble">
+                    <label for="immeuble">Immeuble</label>
+                </div>
+            </div>
+            <div>
+                <label for="nb-bedroom">Nombre de chambre</label>
+                <input type="number" name="nb-bedroom" id="nb-bedroom" value="1">
+            </div>
+            <input type="submit" value="CHERCHER">
+        </form>
     </div>
 </div>
