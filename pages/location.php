8cd8a10cabf2e4547b9f32187677695594562722
scroutch <cecile.duhain@gmail.com>
2022-12-13 10:59:26 +0100

    add page modal and include it in pages

--- pages/location.php
+++ pages/location.php
@@ -13,19 +13,21 @@ $req2->execute();
     <?php
 
     while ($data = $req2->fetch()) {
+        include('pages/modal.php');
     ?>
         <div class="card m-2">
-            <img class="card-img-top" src="./BO_Project/assets/upload/<?php echo $data['img'] ?>" alt="Card image cap">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#modal<?php echo $data['id'] ?>"><img class="card-img-top" src="./BO_Project/assets/upload/<?php echo $data['img'] ?>" alt="Card image cap"></a>
             <div class="card-body d-flex flex-column justify-content-around">
                 <h5 class="card-title d-flex justify-content-between"><?php echo $data['title'] ?><span><?php echo $data['price'] ?> €</span></h5>
-                <p class="card-text d-flex justify-content-center"><i class="fa-solid fa-location-dot"></i><span class="ps-2"><?php echo $data['ville'] ?></span></p>
+                <p class="card-text d-flex justify-content-center"><i class="fa-solid fa-location-dot"></i><span class="ps-2"><?php echo $data['rue'] ?>, <?php echo $data['ville'] ?></span></p>
                 <p class="card-text d-flex justify-content-around">
                     <span class="p-2"><i class="fa-solid fa-bed"></i><span class="ps-2"><?php echo $data['nb_bedroom'] ?></span></span>
                     <span class="p-2"><i class="fa-solid fa-bath"></i><span class="ps-2"><?php echo $data['nb_bathroom'] ?></span></span>
                     <span class="p-2"><i class="fa-solid fa-arrows-up-down-left-right"></i><span class="ps-2"><?php echo $data['surface'] ?> m²</span></span>
                 </p>
             </div>
-            <div class="card-footer d-flex justify-content-end">
+            <div class="card-footer d-flex justify-content-between align-items-center">
+                <a href="pages/ficheProduit.php">En savoir plus</a>
                 <a href="index.php?page=5"><i class="fa-regular fa-envelope p-2"></i></a>
                 <!-- <i class="fa-regular fa-heart p-2"></i> -->
             </div>
