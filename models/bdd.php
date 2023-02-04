546840891aa1b58f1bf48be55083611896817738
scroutch <cecile.duhain@gmail.com>
2023-02-04 11:08:55 +0100

    update bdd.php

--- models/bdd.php
+++ models/bdd.php
@@ -5,7 +5,6 @@ if (!isset($_SESSION)) {
     // var_dump($_SESSION);
 }
 
-
 try {
     // $bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=casanied;charset=utf8', 'root', '');
     $bdd = new PDO('mysql:mysql.hostinger.com;dbname=u548150219_casanied;charset=utf8', 'u548150219_admin', 'Jv7ww6j9!');
