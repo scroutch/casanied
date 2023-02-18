93703989f3505e82d484752593e2eddde6bc3bd5
scroutch <cecile.duhain@gmail.com>
2023-02-18 23:36:31 +0100

    fix label on connect page

--- views/connect.php
+++ views/connect.php
@@ -1,7 +1,6 @@
 <div class="container-fluid col-xl-6 col-md-12 mx-auto mt-5" id="connect">
     <div class="row">
         <?php
-
         if (isset($_SESSION['errorMess'])) {
             echo $_SESSION['errorMess'];
             unset($_SESSION['errorMess']);
@@ -10,11 +9,11 @@
     </div>
     <form method="POST" action="../controlers/connectControler.php">
         <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
+            <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
         </div>
         <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
+            <label for="password" class="form-label">Mot de passe</label>
             <input type="password" class="form-control" id="password" name="password">
         </div>
         <input type="submit" class="btn btn-primary" value="Se connecter"></input>
